204bb0debf3fa5a32e728d72cb1f89985bb8fbb2
Ana Thais Senger <anathaissenger@hotmail.com>
2023-03-01 18:20:14 -0300

    Initial commit

---
+++ src/main/java/com/example/mercado/MercadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercadoApplication.class, args);
+    }
+
+}
