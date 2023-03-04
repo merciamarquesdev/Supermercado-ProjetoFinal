9525ebd1f1205a36810dcf9be97e1bda29ce198e
Ana Thais Senger <anathaissenger@hotmail.com>
2023-03-04 11:10:48 -0300

    repository de cliente

---
+++ src/main/java/com/example/mercado/repositories/ClienteRepository.java
@@ -0,0 +1,6 @@
+package com.example.mercado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
+}
