6c03f5fcd9d70241bfba330c5c63db259887bfdb
Ana Thais Senger <anathaissenger@hotmail.com>
2023-03-04 11:11:27 -0300

    repository de funcionario

---
+++ src/main/java/com/example/mercado/repositories/FuncionarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.mercado.repositories;
+
+import com.example.mercado.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, String> {
+    Optional<Funcionario> findByNomeFuncionario(String nome);
+}
