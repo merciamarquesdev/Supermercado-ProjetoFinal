9b5572fe36b2b5b760e9a5f5bd9eb63711bc19fd
Mercia <68623425+mercietc@users.noreply.github.com>
2023-03-05 14:10:45 -0300

    criacao de services de financeiro

---
+++ src/main/java/com/example/mercado/financeiro/services/SalvarPagamentoService.java
@@ -0,0 +1,50 @@
+package com.example.mercado.financeiro.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Service;
+import com.example.mercado.financeiro.entity.Fornecedor;
+import com.example.mercado.financeiro.entity.Item;
+import com.example.mercado.financeiro.entity.Pagamento;
+import com.example.mercado.financeiro.entity.Produto;
+import com.example.mercado.financeiro.payload.request.ItemRequest;
+import com.example.mercado.financeiro.payload.request.PagamentoFornecedorRequest;
+import com.example.mercado.financeiro.repositories.PagamentoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SalvarPagamentoService {
+
+    private final RetrieveOrCreateProdutoService retrieveOrCreateProdutoService;
+    private final RetrieveOrSaveFornecedorService retrieveOrSaveFornecedorService;
+
+    private final PagamentoRepository pagamentoRepository;
+
+    @CacheEvict(cacheNames = "rlfornecedor", allEntries = true)
+    public void execute(PagamentoFornecedorRequest pagamentoFornecedorRequest){
+        Fornecedor fornecedor = new Fornecedor();
+        BeanUtils.copyProperties(pagamentoFornecedorRequest.getFornecedor(),fornecedor);
+        List<Item> items = pagamentoFornecedorRequest.getItems().stream().map(this::getItems)
+                .collect(Collectors.toList());
+
+        Fornecedor fornecedorSaved = retrieveOrSaveFornecedorService.execute(fornecedor);
+
+        Pagamento pagamento = new Pagamento();
+        pagamento.setFornecedor(fornecedorSaved);
+        pagamento.adicionarItems(items);
+        pagamentoRepository.save(pagamento);
+    }
+
+    private Item getItems(ItemRequest itemRequest) {
+        Item item = new Item();
+        BeanUtils.copyProperties(itemRequest, item);
+        Produto produto = new Produto();
+        BeanUtils.copyProperties(itemRequest.getProduto(), produto);
+        item.setProduto(retrieveOrCreateProdutoService.execute(produto));
+        return item;
+    }
+}
