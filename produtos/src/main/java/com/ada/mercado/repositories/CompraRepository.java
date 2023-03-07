9a905e61b32825efd548194f4707c7ca1c3f41ad
Ana Thais Senger <anathaissenger@hotmail.com>
2023-03-07 20:01:42 -0300

    q isso gente

--- produtos/src/main/java/com/ada/mercado/repositories/CompraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ada.mercado.repositories;
-
-import com.ada.mercado.entity.Compra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-}
